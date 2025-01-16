276bbc784e95c695b90a16f225c123f459972bd5
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2025-01-16 10:08:47 +0330

    💥 Switch to Preact

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import * as visualizer from "rollup-plugin-visualizer";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	plugins: [
+		preact(),
+		visualizer.visualizer({
+			filename: "./dist/bundle-analysis.html",
+			open: true,
+			template: "treemap",
+		}),
+	],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
 });
